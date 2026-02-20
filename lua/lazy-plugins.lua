233ad6c2ac6ed7c51a3243e4fcc947ae8fd0bd37
Kai Nakamura <kaihnakamura@gmail.com>
2026-02-20 09:51:44 -0500

    Show hidden files and gitignored files in tree

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -83,10 +83,11 @@ require('lazy').setup({
           group_empty = true,
         },
         filters = {
-          dotfiles = true,
+          dotfiles = false,
         },
         git = {
           enable = true,
+          ignore = false,
         },
         diagnostics = {
           enable = true,
